ca4b6938b08fb11e805d9c5002790e4f92061442
Phil Wamba <philwamba@gmail.com>
2023-09-10 22:14:10 +0300

    🚀 Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      routes: {
+        '/': (context) => const LoginScreen(title: 'Flutter Sample'),
+        '/register': (context) => const RegisterScreen(title: 'Register'),
+        '/home': (context) => const HomeScreen(title: 'Login'),
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
